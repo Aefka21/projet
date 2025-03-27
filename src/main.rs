fdee67ac38f7b19f0a9ebab00745e1722f246c6e
Xavier Luong <luongxavier77600@gmail.com>
2025-03-27 09:28:30 +0100

    27/03

--- src/main.rs
+++ src/main.rs
@@ -1,145 +1,233 @@
 use bevy::prelude::*;
-//use rand::prelude::*;
 use noise::{NoiseFn, Perlin};
+
+const TILE_SIZE: f64 = 6.;
+const SIDE: usize = 100;
 
 #[derive(PartialEq, Clone)]
 enum TileType {
     Empty,
+    Air,
     Mud,
     Ground,
     Steel,
+    Cave,
+    Player,
+}
+
+#[derive(Resource)]
+struct Mining {
+    is_mining: bool,
 }
 
 struct TileMap {
-    tiles: Vec<Vec<TileType>>,
-    width: usize,
-    height: usize,
+    pub tiles: Vec<Vec<TileType>>,
 }
+
+impl TileMap {
+    pub fn get_tile_type(&self, x: usize, y: usize) -> TileType {
+        self.tiles[y][x].clone()
+    }
+
+    pub fn update_terrain(&mut self, x: usize, y: usize, tile_type: TileType) {
+        self.tiles[y][x] = tile_type; 
+    }
+}
+
+impl Resource for TileMap {}
+
+#[derive(Component)]
+struct Player;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, player_input)
+        //.add_systems(Update, test)
         .run();
 }
 
-fn setup(mut commands: Commands) {
+/*fn test(
+    mut commands: Commands,
+    map: Res<TileMap>,
+    mut mine: ResMut<Mining>,
+    query: Query<(Entity, &Transform), With<Sprite>>
+) {
+    // Calculate total grid size
+    let grid_width = SIDE as f64 * TILE_SIZE;
+    let grid_height = SIDE as f64 * TILE_SIZE;
+    
+    // Center the grid
+    let offset_x = (-grid_width / 2.0 + TILE_SIZE / 2.0) as f32;
+    let offset_y = (-grid_height / 2.0 + TILE_SIZE / 2.0) as f32;
+    let target_position = Vec3::new(offset_x, offset_y + 6. * 32., 0.0);
+    if mine.is_mining {
+        for (entity, transform) in query.iter() {
+            println!("{}", transform.translation);
+            if transform.translation.truncate() == target_position.truncate() {
+                commands.entity(entity).despawn(); // Supprime le sprite
+                println!("Sprite supprimé à {:?}", transform.translation);
+            }
+        }
+        //spawn_tile_grid(&mut commands, &map);
+        mine.is_mining = false;
+    }
+}*/
+
+fn setup(
+   mut commands: Commands,
+) {
     // Add a 2D camera
     commands.spawn((
         Camera2d,
-        Camera {
-            hdr: true, // HDR is required for the bloom effect
-            ..default()
-        },
-        //Bloom::default(),
     ));
 
-    
     // Generate noise map
-    let width = 100;
-    let height = 100;
-    let scale = 5.0;
-    let tile_map = generate_tile_map(width, height, scale);
+    let scale = 2.5;
+    let tile_map = generate_tile_map(scale);
     
     // Create a grid of sprites
     spawn_tile_grid(&mut commands, &tile_map);
+    commands.insert_resource(tile_map);
+
+    commands.insert_resource(Mining {is_mining: false});
+
+    // Calculate total grid size
+    let grid_width = SIDE as f64 * TILE_SIZE;
+    let grid_height = SIDE as f64 * TILE_SIZE;
+    
+    // Center the grid
+    let offset_x = (-grid_width / 2.0 + TILE_SIZE / 2.0) as f32;
+    let offset_y = (-grid_height / 2.0 + TILE_SIZE / 2.0) as f32;
+
+    commands.spawn((
+        Player,
+        Transform::from_xyz(offset_x + 6., offset_y + 32. * 6., 0.0),
+        GlobalTransform::default(),
+        Sprite {
+            color: Color::srgb(1., 0., 0.),
+            custom_size: Some(Vec2::splat(TILE_SIZE as f32 * 2.)),
+            ..Default::default()
+        },
+    ));
 }
 
-fn generate_tile_map(width: usize, height: usize, scale: f64) -> TileMap {
-    //let rng = rand::rng();
-    let perlin = Perlin::new(43);
-    let mut noise_array = vec![vec![TileType::Empty; width]; height];
+fn generate_tile_map(scale: f64) -> TileMap {
+    let perlin = Perlin::new(42);
+    let mut array = vec![vec![TileType::Empty; SIDE]; SIDE];
     
-    for y in 0..height {
-        for x in 0..width {
+    for y in 0..SIDE {
+        for x in 0..SIDE {
             // Convert grid coordinates to appropriate noise coordinates
-            let nx = x as f64 / width as f64 * scale;
-            let ny = y as f64 / height as f64 * scale;
+            let nx = x as f64 / SIDE as f64 * scale;
+            let ny = y as f64 / SIDE as f64 * scale;
             
             // Get noise value and scale from [-1, 1] to [0, 1]
             let noise_value = perlin.get([nx, ny]) as f64;
-            /*if noise_value > 0.5 {
-                println!("Steel!!!");
-            }*/
-            noise_array[y][x] = get_tile((noise_value + 1.0) / 2.0);
+            if (y == 0 && x == 0) || (y == 1 && x == 0) {
+                array[y][x] = TileType::Player;
+            } else if y > SIDE - 20 {
+                array[y][x] = TileType::Air;
+            } else {
+                array[y][x] = get_tile((noise_value + 1.0) / 2.0);
+            }
         }
     }
     
-    TileMap{tiles: noise_array, width: width, height: height}
+    TileMap{tiles: array}
 }
 
 fn get_tile(val: f64) -> TileType {
     match val.abs() {
-        v if v < 0.2 => TileType::Mud,
-        v if v < 0.7 => TileType::Ground,
+        v if v < 0.3 => TileType::Mud,
+        v if v < 0.6 => TileType::Ground,
+        v if v < 0.8 => TileType::Cave,
         _ => TileType::Steel,
     }
 }
 
-fn print_noise_array(array: &Vec<Vec<TileType>>) {
-    let height = array.len();
-    let width = if height > 0 { array[0].len() } else { 0 };
-    
-    println!("Perlin Noise Array ({}x{}):", width, height);
-    for y in 0..30 /* height */ {
-        for x in 0..30 /* width */{
-            let tile = array[y][x].clone();
-            // Format to 4 decimal places for readability
-            if tile == TileType::Mud {
-                print!("0 ");
-            } else if tile == TileType::Ground {
-                print!("1 ");
-            } else {
-                print!("2 ");
-            }
-        }
-        println!(); // New line after each row
+fn get_color(tile: TileType) -> Color {
+    match tile {
+        TileType::Mud => Color::srgb(0.8, 0.4, 0.16),
+        TileType::Ground => Color::srgb(0.6, 0.2, 0.2),
+        TileType::Steel => Color::srgb(0.8, 0.8, 0.9),
+        TileType::Empty => Color::srgb(0., 0., 0.),
+        TileType::Air => Color::srgb(0.6, 0.6, 1.),
+        TileType::Cave => Color::srgb(0.2, 0.0, 0.01),
+        TileType::Player => Color::srgb(0., 1., 0.),
     }
 }
 
 fn spawn_tile_grid(commands: &mut Commands, tile_map: &TileMap) {
-    // Define the size of each tile
-    let tile_size = 6.0;
-    
     // Calculate total grid size
-    let grid_width = tile_map.width as f64 * tile_size;
-    let grid_height = tile_map.height as f64 * tile_size;
+    let grid_width = SIDE as f64 * TILE_SIZE;
+    let grid_height = SIDE as f64 * TILE_SIZE;
     
     // Center the grid
-    let offset_x = -grid_width / 2.0 + tile_size / 2.0;
-    let offset_y = -grid_height / 2.0 + tile_size / 2.0;
+    let offset_x = -grid_width / 2.0 + TILE_SIZE / 2.0;
+    let offset_y = -grid_height / 2.0 + TILE_SIZE / 2.0;
     
     // Spawn a sprite for each tile
-    for y in 0..tile_map.height {
-        for x in 0..tile_map.width {
-            let tile_type = &tile_map.tiles[y][x];
+    for y in 0..SIDE {
+        for x in 0..SIDE {
+            let tile_type = tile_map.tiles[y][x].clone();
             
             // Choose color based on tile type
-            let color = match tile_type {
-                TileType::Mud => Color::srgb(0., 0., 1.),
-                TileType::Ground => Color::srgb(1., 0., 0.),
-                TileType::Steel => Color::srgb(1., 1., 1.),
-                TileType::Empty => Color::srgb(0., 0., 0.),
-            };
+            let color = get_color(tile_type);
             
             // Calculate position
             let position = Vec3::new(
-                (offset_x + x as f64 * tile_size) as f32,
-                (offset_y + y as f64 * tile_size) as f32,
+                (offset_x + x as f64 * TILE_SIZE) as f32,
+                (offset_y + y as f64 * TILE_SIZE) as f32,
                 0.0,
             );
             
+            // Spawn the tile
             commands.spawn((
-                // Transform will automatically add GlobalTransform
                 Transform::from_translation(position),
-                // Default visibility components will be added automatically
                 Visibility::Inherited,
                 Sprite {
                     color,
-                    custom_size: Some(Vec2::new(tile_size as f32, tile_size as f32)),
+                    custom_size: Some(Vec2::new(TILE_SIZE as f32, TILE_SIZE as f32)),
                     ..default()
                 },
             ));
         }
+    }
+}
+
+fn player_input(
+    key: Res<ButtonInput<KeyCode>>,
+    mut query: Query<&mut Transform, With<Player>>,
+    mut tile_map: ResMut<TileMap>,
+    mut mining: ResMut<Mining>,
+    //commands: &mut Commands,
+) {
+    for mut transform in query.iter_mut() {
+        let mut movement = Vec2::ZERO;
+
+        if key.pressed(KeyCode::Space) {
+            /*for y in 0..SIDE {
+                for x in 0..SIDE {
+                    if tile_map.get_tile_type(x, y) == TileType::Ground {
+                        tile_map.update_terrain(x, y, TileType::Cave);
+                        mining.is_mining = true;
+                    }
+                }
+            }*/
+        }
+        if key.pressed(KeyCode::KeyA) {
+            movement.x -= TILE_SIZE as f32; // Left
+        }
+        if key.pressed(KeyCode::KeyD) {
+            movement.x += TILE_SIZE as f32; // Right
+        }
+
+        // Appliquer le mouvement (case par case)
+        transform.translation.x += movement.x;
+        transform.translation.y += movement.y;
+
+        transform.translation.x = transform.translation.x.clamp((-TILE_SIZE * SIDE as f64 / 2.) as f32 + TILE_SIZE as f32, (TILE_SIZE * SIDE as f64 / 2.) as f32 - TILE_SIZE as f32);
     }
 }
